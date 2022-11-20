5e0356229f432752b395eb4bc3055a2d73af6ba5
Abhishek Rana <abhishekrana21092003@gmail.com>
2022-11-20 20:18:05 +0530

    Initial Commit

---
+++ CodeChef/Self Learning/Math/CHEFBARBER.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define el "\n";
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout<<fixed<<setprecision(2);
+	long t,n,m;
+	cin>>t;
+	while(t--){
+	    cin>>n>>m;
+	    cout<<n*m<<el;
+	}
+	return 0;
+}
