6cbd14c0c7be29b61b5a249d3ef99fef24426fa4
Abhishek Rana <abhishekrana21092003@gmail.com>
2022-11-23 23:14:02 +0530

    Initial Commit

---
+++ CodeChef/Self Learning/Math/RATING.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define el "\n";
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout<<fixed<<setprecision(2);
+	long t,s;
+	cin>>t;
+	while(t--){
+	    cin>>s;
+	    cout<<-(s+1)<<el;
+	}
+	return 0;
+}
