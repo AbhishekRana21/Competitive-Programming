481852fcc7b861f29a93d9f2b16395a7b7b18914
Abhishek Rana <abhishekrana21092003@gmail.com>
2022-11-24 15:13:32 +0530

    Initial Commit

---
+++ CodeChef/Self Learning/Math/UWCOI21A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define el "\n";
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout<<fixed<<setprecision(2);
+	long t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<1<<" "<<n<<el;
+	}
+	return 0;
+}
